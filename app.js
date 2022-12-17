edeee7144e60b89bf5097affccfee0c93c74fa0a
Harshit Choudhary <harshit1840077@gmail.com>
2022-12-17 11:34:03 +0530

    set view engine implementing ejs

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const port = process.env.PORT||3000;
 const moment = require('moment')
 app.locals.moment = moment;
 
+// template engine  
+app.use(express.static('public'))
+app.set('view engine','ejs')
+
 app.use(express.urlencoded({ extended: true }));
 app.use('/',require('./routes/news'))
+
+app.set('views','./views')
 
 app.listen(port,()=> console.log("started"))
