537c27b5256e0b2e7715369a1d7c6ea1777f762c
Harshit Choudhary <harshit1840077@gmail.com>
2022-12-17 11:24:51 +0530

    setting up server file

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app=express()
+const port = process.env.PORT||3000;
+//const bodyParser = require('body-parser');
+const moment = require('moment')
+app.locals.moment = moment;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port,()=> console.log("started"))
